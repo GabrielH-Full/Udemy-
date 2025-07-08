39f24dd491c811ad7f4a5a627aeef34a52f8ae77
GabrielH-Full <gabrielhb2020@gmail.com>
2025-07-08 15:44:07 -0300

    senha fixa att

--- src/EstruturaRepetitivas/senha.java
+++ src/EstruturaRepetitivas/senha.java
@@ -1,4 +1,21 @@
 package EstruturaRepetitivas;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class senha {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite a senha: ");
+        int senha = sc.nextInt();
+
+        while (senha != 2312) {
+            System.out.printf("Senha incorreta! Tente novamente:");
+            senha = sc.nextInt();
+        }
+        System.out.println("Acesso Permitido!");
+        sc.close();
+    }
 }
