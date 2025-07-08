9d40a1892ad23d67b9138f67a21f8e9f8fbf5f68
GabrielH-Full <gabrielhb2020@gmail.com>
2025-07-08 17:12:35 -0300

    att

--- src/EstruturaRepetitivas/pares_consecutivos.java
+++ src/EstruturaRepetitivas/pares_consecutivos.java
@@ -1,4 +1,37 @@
 package EstruturaRepetitivas;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class pares_consecutivos {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n1;
+        System.out.println("Digite um numero inteiro: ");
+        n1 = sc.nextInt();
+
+        while (n1 != 0) {
+            int soma = 0;
+            int proximo = n1 + 2;
+            int contador = 0;
+
+            while (contador < 5) {
+                if ((n1 % 2) != 0) {
+                    n1++;
+                }
+                else {
+                    soma += proximo;
+                    contador++;
+                }
+                proximo++;
+            }
+            System.out.printf("SOMA = %d\n", soma);
+            System.out.print("Digite um numero inteiro: ");
+            n1 = sc.nextInt();
+        }
+
+    }
 }
+
